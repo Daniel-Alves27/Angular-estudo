c6a2baf6235f1536905c0b54ee13356c64fe30e5
Daniel <daniel502121@gmail.com>
2024-09-26 20:50:10 -0300

    paginação, botão de favorito e barra de pesquisa

--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -1,6 +1,7 @@
 import { PensamentoService } from './../pensamento.service';
 import { Component, OnInit } from '@angular/core';
 import { Pensamento } from '../pensamento';
+import { Router, RouteReuseStrategy } from '@angular/router';
 
 @Component({
   selector: 'app-listar-pensamento',
@@ -11,12 +12,61 @@ export class ListarPensamentoComponent implements OnInit {
 
   listaPensamentos: Pensamento[] = [];
 
-  constructor(private service: PensamentoService) { }
+  constructor(
+    private service: PensamentoService,
+    private router: Router
+  ) { }
+
+  paginaAtual: number = 1;
+  haMaisPensamentos: boolean = true;
+  filtro: string = '';
+  favorito: boolean = false;
+  listaFavoritos: Pensamento[] = []
+  titulo: string = 'Meu Mural'
 
   ngOnInit(): void {
-    this.service.listar().subscribe((listaPensamentos) => {
+    this.service.listar(this.paginaAtual,this.filtro,this.favorito).subscribe((listaPensamentos) => {
       this.listaPensamentos = listaPensamentos
     })
   }
+
+  carregarMaisPensamentos() {
+    this.service.listar(++this.paginaAtual, this.filtro, this.favorito).subscribe( listaPensamentos => {
+      this.listaPensamentos.push(...listaPensamentos)
+      if(!listaPensamentos.length){
+        this.haMaisPensamentos = false
+      }
+    })
+  }
+
+  pesquisarPensamentos() {
+    this.haMaisPensamentos = true
+    this.paginaAtual = 1
+    this.service.listar(this.paginaAtual, this.filtro, this.favorito).subscribe(listaPensamentos => {
+      this.listaPensamentos = listaPensamentos
+    })
+  }
+
+  recarregarComponent() {
+    this.favorito = false
+    this.paginaAtual = 1
+
+    this.router.navigate([this.router.url])
+    this.router.onSameUrlNavigation = 'reload'
+    this.router.routeReuseStrategy.shouldReuseRoute = () => false
+  }
+
+
+  mostrarFavoritos() {
+    this.titulo = 'Meus favorito'
+    this.favorito = true
+    this.haMaisPensamentos = true
+    this.paginaAtual = 1
+    this.service.listar(this.paginaAtual, this.filtro,this.favorito).subscribe(listaDeFavorito => {
+      this.listaPensamentos = listaDeFavorito
+      this.listaFavoritos = listaDeFavorito
+    })
+  }
 }
+
 
--- src/app/componentes/pensamentos/pensamento.service.ts
+++ src/app/componentes/pensamentos/pensamento.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Pensamento } from './pensamento';
 import { Observable } from 'rxjs';
+import { Params } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
@@ -10,11 +11,29 @@ export class PensamentoService {
 
   private readonly API = 'http://localhost:3000/pensamentos'
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
-  listar(): Observable<Pensamento[]> {
-    return this.http.get<Pensamento[]>(this.API)
+  listar(pagina: number, filtro: string, favorito: boolean): Observable<Pensamento[]> {
+
+    const itensPorPagina: number = 6;
+
+    let params = new HttpParams()
+    .set("_page", pagina)
+    .set("_limit", itensPorPagina)
+
+    if(filtro.trim().length > 2){
+      params = params.set("q",filtro)
+    }
+
+    if(favorito) {
+      params = params.set("favorito", true)
+    }
+
+    //GET /posts?q=internet
+    //GET /posts?_page=1&_per_page=25
+    return this.http.get<Pensamento[]>(this.API, { params })
   }
+
 
   criar(pensamento: Pensamento): Observable<Pensamento> {
     return this.http.post<Pensamento>(this.API, pensamento)
@@ -23,7 +42,11 @@ export class PensamentoService {
   editar(pensamento: Pensamento): Observable<Pensamento> {
     const url = `${this.API}/${pensamento.id}`
     return this.http.put<Pensamento>(url, pensamento )
+  }
 
+  mudarFavorito(pensamento: Pensamento): Observable<Pensamento> {
+    pensamento.favorito = !pensamento.favorito;
+    return this.editar(pensamento)
   }
 
   excluir(id: number): Observable<Pensamento> {
